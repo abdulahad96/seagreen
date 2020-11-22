e991dbe6893f0c8c083e95f3b51e8af449b3e480
Muhammad Salahuddin <msalahuddin8890@gmail.com>
2020-11-22 16:31:22 +0500

    code refactoring

--- src/containers/Notification/styles.js
+++ src/containers/Notification/styles.js
@@ -3,7 +3,7 @@ import {Metrics, Colors, Fonts} from '../../theme';
 
 export default StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   mainHeading: {
     fontSize: Metrics.ratio(16),
@@ -12,39 +12,55 @@ export default StyleSheet.create({
   },
   declineButtonView: {
     width: '45%',
-    height:Metrics.ratio(50),
-    marginTop: Metrics.ratio(40),
+    marginTop: Metrics.ratio(10),
     marginBottom: Metrics.ratio(10),
-    borderRadius:Metrics.ratio(25),
-    marginHorizontal:Metrics.screenWidth*0.02,
+    borderRadius: Metrics.ratio(25),
+    marginHorizontal: Metrics.screenWidth * 0.02,
     borderColor: Colors.footer_green,
-    borderWidth:Metrics.ratio(1),
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    // elevation: 4
+    borderWidth: Metrics.ratio(1),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingVertical: Metrics.ratio(5)
+   
   },
- declineBtnTxt: {
+  declineBtnTxt: {
     color: Colors.footer_green,
     fontSize: Metrics.ratio(16),
-      fontFamily: Fonts.type.demibold,
+    fontFamily: Fonts.type.demibold,
   },
   acceptButtonView: {
     width: '45%',
-    height:Metrics.ratio(50),
-    marginTop: Metrics.ratio(40),
-    marginHorizontal:Metrics.screenWidth*0.02,
+    paddingVertical: Metrics.ratio(5),
+    marginTop: Metrics.ratio(10),
+    marginHorizontal: Metrics.screenWidth * 0.02,
     marginBottom: Metrics.ratio(10),
-    borderRadius:Metrics.ratio(25),
+    borderRadius: Metrics.ratio(25),
     backgroundColor: Colors.footer_green,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    elevation: 4
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    elevation: 4,
   },
- acceptBtnTxt: {
+  acceptBtnTxt: {
     color: Colors.white,
     fontSize: Metrics.ratio(16),
-      fontFamily: Fonts.type.demibold,
-  }
+    fontFamily: Fonts.type.demibold,
+  },
+  subContainer: {
+    paddingHorizontal: Metrics.screenWidth * 0.05,
+    paddingVertical: Metrics.screenWidth * 0.05,
+  },
+  cardContainer: {
+    backgroundColor: Colors.white,
+    borderRadius: Metrics.ratio(10),
+    paddingVertical: Metrics.ratio(10),
+    paddingHorizontal: Metrics.ratio(10),
+  },
+  smallText: {
+    fontSize: Fonts.size.ten,
+  },
+  LargeText: {
+    fontSize: Fonts.size.sixteen,
+  },
 });
